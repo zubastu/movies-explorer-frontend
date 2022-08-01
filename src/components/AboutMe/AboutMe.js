2808e07d4ba7c896bdb53fd59d172b7fb560964d
zubastu <79757684+zubastu@users.noreply.github.com>
2022-08-01 14:27:51 +0300

    Обновил футер

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./AboutMe.css";
+import MainTitle from "../MainTitle/MainTitle";
 
 const AboutMeAboutMe = () => {
-  return <div className="about-me" id="student-section"></div>;
+  return (
+    <div className="about-me" id="student-section">
+      <MainTitle text="Студент" />
+      <div className="about-me__description">
+        <h3 className="about-me__title">Илья</h3>
+        <p className="about-me__subtitle">Фронтенд-разработчик, 27 лет</p>
+        <p className="about-me__text">
+          Я родился в Северодвинске и живу в Балашихе, закончил Колледж
+          Автомобильного Транспорта №9. Я люблю слушать музыку, а ещё увлекаюсь
+          бегом. Недавно начал кодить. Это мой дипломный проект, в дальнейшем
+          планирую развиваться в данном направлении.
+        </p>
+        <img className="about-me__photo" alt="Мое фото" />
+        <a href="https://facebook.com/">Facebook</a>
+        <a href="https://github.com/zubastu">Github</a>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMeAboutMe;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Portfolio.css";
 
 const Portfolio = () => {
-  return <div></div>;
+  return (
+    <div className="portfolio">
+      <h5 className="portfolio__heading">Портфолио</h5>
+      <ul className="links">
+        <li className="links__item">Статичный сайт</li>
+        <li className="links__item">Адаптивный сайт</li>
+        <li className="links__item">Одностраничное приложение</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Portfolio;

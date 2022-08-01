cb4732141e4f225514fbd5cd1f7b2c15072697a7
zubastu <79757684+zubastu@users.noreply.github.com>
2022-08-01 13:48:19 +0300

    Компонент Технологии(не адаптив)

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Techs.css";
+import MainTitle from "../MainTitle/MainTitle";
 
 const Techs = () => {
-  return <div className="techs" id="tech-section"></div>;
+  return (
+    <div className="techs" id="tech-section">
+      <MainTitle text="Технологии" />
+      <div className="tech__content">
+        <div className="techs__text-content">
+          <h2 className="techs__title">7 технологий</h2>
+          <p className="techs__subtitle">
+            На курсе веб-разработки мы освоили технологии, которые применили в
+            дипломном проекте.
+          </p>
+        </div>
+
+        <ul className="technologies">
+          <li className="technologies__item">HTML</li>
+          <li className="technologies__item">CSS</li>
+          <li className="technologies__item">JS</li>
+          <li className="technologies__item">React</li>
+          <li className="technologies__item">Git</li>
+          <li className="technologies__item">Express.js</li>
+          <li className="technologies__item">mongoDB</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Techs;

3916fb8b6d0f50ad3d3c7d14f5fcc0925d41d1f0
zubastu <79757684+zubastu@users.noreply.github.com>
2022-08-01 12:07:10 +0300

    Компонент О проекте, компонент тайтла для мэйна(не адаптив)

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,25 +1,34 @@
 import React from "react";
 import "./AboutProject.css";
+import MainTitle from "../MainTitle/MainTitle";
 
 const AboutProject = () => {
   return (
     <div className="about-project" id="about-section">
-      <h3>О проекте</h3>
+      <MainTitle text="О проекте" />
+
       <div className="about-project__content">
         <h4 className="about-project__heading">
           Дипломный проект включал 5 этапов
+        </h4>
+        <h4 className="about-project__heading">
+          На выполнение диплома ушло 5 недель
         </h4>
         <p className="about-project__subheading">
           Составление плана, работу над бэкендом, вёрстку, добавление
           функциональности и финальные доработки.
         </p>
-        <h4 className="about-project__heading">
-          На выполнение диплома ушло 5 недель
-        </h4>
         <p className="about-project__subheading">
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
           соблюдать, чтобы успешно защититься.
         </p>
+      </div>
+
+      <div className="time-line">
+        <span className="time-line__week-first">1 неделя</span>
+        <span className="time-line__weeks-others">4 недели</span>
+        <span className="time-line__stack-backend">Back-end</span>
+        <span className="time-line__stack-frontend">Front-end</span>
       </div>
     </div>
   );

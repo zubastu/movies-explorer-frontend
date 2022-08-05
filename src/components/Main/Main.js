064bb4063ea49dfbb8c9c3e0ad564b935356d43b
zubastu <79757684+zubastu@users.noreply.github.com>
2022-08-05 21:46:10 +0300

    Портфолио адаптив, отправка на проверку номер 1

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -15,7 +15,7 @@ const Main = ({ setMenuActive }) => {
       <AboutProject />
       <Techs />
       <AboutMe />
-      {/*      <Portfolio />*/}
+      <Portfolio />
       <Footer />
     </>
   );
